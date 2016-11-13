e20d75ef3b435c9da68d6e19dc225b3416f06ed4
00161615 <00161615@nbdti1015944366.lojasrenner.com.br>
2016-11-13 16:54:25 -0200

    V-Final Swing

--- SistemaBar/src/Persistencia/ClienteTxtFile.java
+++ SistemaBar/src/Persistencia/ClienteTxtFile.java
@@ -14,12 +14,12 @@ import java.util.logging.Logger;
  * @author Bernardo Copstein
  */
 public class ClienteTxtFile {
-    
-    private static final String arquivoEntrada = "C:\\Users\\l61615\\Documents\\NetBeansProjects\\SistemaBAR\\entrada.dat";
-    private static final String arquivoSaida = "C:\\Users\\l61615\\Documents\\NetBeansProjects\\SistemaBAR\\saida.dat";
+
+    private static final String arquivoEntrada = "entrada.dat";
+    private static final String arquivoSaida = "saida.dat";
     private List<Cliente> listaCliente;
-   
-    
+
+
     private void saveDataEntrada() throws IOException {
        FileWriter fw = new FileWriter(arquivoEntrada);
         for (Cliente c : listaCliente) {
@@ -28,7 +28,7 @@ public class ClienteTxtFile {
         }
         fw.close();
     }
-    
+
     private void loadDataEntrada() throws IOException {
         FileReader fr = new FileReader(arquivoEntrada);
         BufferedReader br = new BufferedReader(fr);
@@ -37,19 +37,19 @@ public class ClienteTxtFile {
             StringTokenizer st = new StringTokenizer(line, ",");
             String nome = st.nextToken();
             String cpf = st.nextToken();
-            
+
             int idade = 0;
-            
+
             try {
                 idade = Integer.parseInt(st.nextToken());
-                
+
             } catch (NumberFormatException e) {
                 throw new IOException("Formato de dados inválido", e);
             }
             String sexo = st.nextToken();
             String tipoCliente = st.nextToken();
             String categoria = st.nextToken();
-            
+
             try {
                 Cliente clnt = new Cliente(nome, cpf, sexo, idade, tipoCliente, categoria);
                 listaCliente.add(clnt);
@@ -60,7 +60,7 @@ public class ClienteTxtFile {
         }
         br.close();
     }
-    
+
     private void saveDataSaida(Cliente clnt) throws IOException {
         FileWriter fw = new FileWriter(arquivoSaida);
         for (Cliente c : listaCliente) {
@@ -70,7 +70,7 @@ public class ClienteTxtFile {
         }
         fw.close();
     }
-    
+
     private void loadDataSaida() throws IOException {
         FileReader fr = new FileReader(arquivoEntrada);
         BufferedReader br = new BufferedReader(fr);
@@ -79,19 +79,19 @@ public class ClienteTxtFile {
             StringTokenizer st = new StringTokenizer(line, ",");
             String nome = st.nextToken();
             String cpf = st.nextToken();
-            
+
             int idade = 0;
-            
+
             try {
                 idade = Integer.parseInt(st.nextToken());
-                
+
             } catch (NumberFormatException e) {
                 throw new IOException("Formato de dados inválido", e);
             }
             String sexo = st.nextToken();
             String tipoCliente = st.nextToken();
             String categoria = st.nextToken();
-            
+
             try {
                 Cliente clnt = new Cliente(nome, cpf, sexo, idade, tipoCliente, categoria);
                 listaCliente.add(clnt);
@@ -114,29 +114,29 @@ public class ClienteTxtFile {
             loadDataSaida();
         }*/
     }
- 
-  
+
+
     public void add(Cliente cliente) {
         listaCliente.add(cliente);
         try {
             saveDataEntrada();
         } catch (IOException ex) {
             Logger.getLogger(ClienteTxtFile.class.getName()).log(Level.SEVERE, null, ex);
-        }        
+        }
     }
-    
-   
+
+
     public void removeCliente(Cliente cliente) throws DAOException {
         try {
-         
+
             saveDataSaida(cliente);
         } catch (IOException mensagem) {
            throw new DAOException("Erro ao salvar arquivo!");
-        }        
+        }
         listaCliente.remove(cliente);
     }
 
-    
+
 
     public Cliente pesquisaClienteCPF(String cpf) {
         for (Cliente c : listaCliente) {
@@ -146,8 +146,8 @@ public class ClienteTxtFile {
         }
         return null;
     }
-   
 
-   
-    
+
+
+
 }
